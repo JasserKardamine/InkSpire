4646f879627b13ce20049c35de234e1669b986ef
ayoub chouat <ayoub.chouat@esprit.tn>
2025-02-19 12:52:42 +0100

    adding user/auction/bid entities + updating gitignore with migration folder and .env files

---
+++ src/Entity/Auction.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuctionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuctionRepository::class)]
+class Auction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 59)]
+    private ?string $label = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startDate = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
+
+    #[ORM\Column]
+    private ?float $startPrice = null;
+
+    #[ORM\Column]
+    private ?float $endPrice = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $status = null;
+
+    /**
+     * @var Collection<int, Bid>
+     */
+    #[ORM\OneToMany(targetEntity: Bid::class, mappedBy: 'auction', orphanRemoval: true)]
+    private Collection $bids;
+
+    public function __construct()
+    {
+        $this->bids = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): static
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): static
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getStartPrice(): ?float
+    {
+        return $this->startPrice;
+    }
+
+    public function setStartPrice(float $startPrice): static
+    {
+        $this->startPrice = $startPrice;
+
+        return $this;
+    }
+
+    public function getEndPrice(): ?float
+    {
+        return $this->endPrice;
+    }
+
+    public function setEndPrice(float $endPrice): static
+    {
+        $this->endPrice = $endPrice;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Bid>
+     */
+    public function getBids(): Collection
+    {
+        return $this->bids;
+    }
+
+    public function addBid(Bid $bid): static
+    {
+        if (!$this->bids->contains($bid)) {
+            $this->bids->add($bid);
+            $bid->setAuction($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBid(Bid $bid): static
+    {
+        if ($this->bids->removeElement($bid)) {
+            if ($bid->getAuction() === $this) {
+                $bid->setAuction(null);
+            }
+        }
+        
+        return $this;
+    }
+}
